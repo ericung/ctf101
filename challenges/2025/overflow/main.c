00108a42d07293bae8ea21387f7ab4f2901f8ad4
ericung <ungericw@tutanota.com>
2025-04-30 16:39:11 -0500

    overflow

---
+++ challenges/2025/overflow/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+typedef struct n132{
+    char buf[0x18];
+    size_t overflow;
+} n132;
+int init(){
+    fclose(stderr);
+    setvbuf(stdin,  0, 2, 0);
+    setvbuf(stdout, 0, 2, 0);
+}
+int main(){
+    init();
+    n132 node;
+    memset(&node,0,sizeof(node));
+    puts("You are allowed to input 24 bytes:");
+    read(0,node.buf, sizeof(node));
+    if(node.overflow){
+        system("/bin/sh");
+    }
+    else{
+        puts("bye bye");
+    }
+}
